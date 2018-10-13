4a643bfcf3b33b04fec44de2e5af0dca57761568
bdzzh-3D <bdzzh2002@163.com>
2018-10-13 12:14:39 +0800

    Rebuild终于成功了

--- app/src/main/java/com/zzh/blunocoin/tool/Msg.java
+++ app/src/main/java/com/zzh/blunocoin/tool/Msg.java
@@ -2,7 +2,7 @@ package com.zzh.blunocoin.tool;
 import android.content.*;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.*;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.view.*;
 
